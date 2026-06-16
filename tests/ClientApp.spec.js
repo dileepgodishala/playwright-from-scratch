f640e7bad52de07695fe3e65288cba85029b93dc
dileepgodishala <godishaladileep@gmail.com>
2026-06-16 20:05:32 +0530

    add scripts

---
+++ tests/ClientApp.spec.js
@@ -0,0 +1,20 @@
+const {test, page, expect} = require('@playwright/test');
+
+test('Assignment - 1', async({page}) =>
+{
+    await page.goto("https://rahulshettyacademy.com/client/#/auth/login");
+
+    const email = page.locator("#userEmail");
+    const password = page.locator("#userPassword");
+    const loginButton = page.locator("#login");
+
+    await email.fill("godishaladileep@gmail.com");
+    await password.fill("Test@123");
+    await loginButton.click();
+
+    await page.waitForLoadState("networkidle");
+
+    const firstItem = page.locator(".card-body b").allTextContents();
+    console.log(firstItem);
+
+});
